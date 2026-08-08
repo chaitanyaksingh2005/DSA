83d78f0d792322bda167af3520fde05ccb887f6e
chaitanyaksingh2005 <chaitanyaksingh2005@gmail.com>
2026-08-08 15:54:13 +0530

    Time: 2 ms (35.86%), Space: 9.7 MB (94.5%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,26 +1,25 @@
-
 class Solution {
 public:
-    bool valid(const string& s, int i, int j) {
-        if (i >= j) {
+    bool helper(const string &s, int start, int end) {
+        if (start >= end) {
             return true;
         }
-        
-        if (!isalnum(s[i])) {
-            return valid(s, i + 1, j);
+        if (!isalnum(s[start])) {
+            return helper(s, start + 1, end);
         }
-        if (!isalnum(s[j])) {
-            return valid(s, i, j - 1);
-
+        if (!isalnum(s[end])) {
+            return helper(s, start, end - 1);
         }
-        if (tolower( s[i]) != tolower (s[j])) {
+        if (tolower(s[start]) != tolower(s[end])) {
             return false;
         }
-        return valid(s,i+1,j-1);
+
+        return helper(s, start + 1, end - 1);
     }
 
-    bool isPalindrome(string s)
-     {
-     return valid(s, 0, s.size() - 1);
-       }
+    bool isPalindrome(string s) {
+        int start = 0;
+        int end = s.size() - 1;
+        return helper(s, start, end);
+    }
 };
