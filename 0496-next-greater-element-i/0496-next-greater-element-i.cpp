abfbfaa2d4e20b211168d1c8e228766cd310ac37
chaitanyaksingh2005 <chaitanyaksingh2005@gmail.com>
2026-08-25 22:35:43 +0530

    Time: 0 ms (100%), Space: 12.9 MB (39.65%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        
+        stack<int> st;
+        unordered_map<int, int> mp;
+        vector<int> ans(nums1.size(), -1);
 
-        vector<int> ans;
+        // Find next greater element for every element of nums2
+        for (int i = nums2.size() - 1; i >= 0; i--) {
 
-        for (int i = 0; i < nums1.size(); i++) {
-            int greater = -1;
-            for (int j = 0; j < nums2.size(); j++) {
-                if (nums1[i] == nums2[j]) {
-                    for (int k = j + 1; k < nums2.size(); k++) {
-                        if (nums2[k] > nums2[j]) {
-                            greater = nums2[k];
-                            break;
-                        }
-                    }
-                    break;
-                }
+            while (!st.empty() && st.top() <= nums2[i]) {
+                st.pop();
             }
 
-            ans.push_back(greater);
+            if (st.empty()) {
+                mp[nums2[i]] = -1;
+            }
+            else {
+                mp[nums2[i]] = st.top();
+            }
+
+            st.push(nums2[i]);
+        }
+
+        // Get answers for nums1
+        for (int i = 0; i < nums1.size(); i++) {
+            ans[i] = mp[nums1[i]];
         }
 
         return ans;
