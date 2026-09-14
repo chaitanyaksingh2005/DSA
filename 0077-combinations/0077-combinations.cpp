9f8ca7bcc7eb5d64c7f98e506225f4ee06b730ba
chaitanyaksingh2005 <chaitanyaksingh2005@gmail.com>
2026-09-14 19:32:22 +0530

    Time: 71 ms (46.35%), Space: 82.6 MB (27.02%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,31 +1,28 @@
-
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     vector<vector<int>> ans;
+    void helper(int i, int n, int k, vector<int>& temp)
 
-    void solve(int n, int k, vector<int>& temp, int start) {
+    {
+
         if (k == 0) {
             ans.push_back(temp);
             return;
         }
 
-        if (start > n || (n - start + 1) < k) {
-            return;
-        }
-        for (int i = start; i <= n; i++) {
-            temp.push_back(i);
-            solve(n, k - 1, temp, i + 1);
-            temp.pop_back(); // Backtrack
+        if (k > 0) {
+
+            for (int j = i; j <= n; j++) {
+                temp.push_back(j);
+                helper(j + 1, n, k - 1, temp);
+                temp.pop_back();
+            }
         }
     }
-
     vector<vector<int>> combine(int n, int k) {
-        ans.clear();
         vector<int> temp;
-        solve(n, k, temp, 1);
+        int i = 1;
+        helper(1, n, k, temp);
         return ans;
     }
 };
